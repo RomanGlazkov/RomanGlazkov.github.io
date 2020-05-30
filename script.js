a0d53e3460ffd26efff82d0306cf2ce3687c9451
RomanGlazkov <66161402+RomanGlazkov@users.noreply.github.com>
2020-05-30 21:12:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-let requestURL = 'https://api.github.com/users';
-let request = new XMLHttpRequest();
+var requestURL = 'https://api.github.com/users';
+var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-request.onload = () => {
-    let usersList = request.response;
-    avatar(usersList);
-    account(usersList);
-    link(usersList);
+request.onload = function() {
+  var users = request.response;
+  userCard(users);
 };
 
-let header = document.querySelector('.profile_1');
-const account = (jsonObj) => {
-  let type = document.createElement('h2');
-  type.textContent = jsonObj[0]['type'];
-  header.appendChild(type);
+var container = document.querySelector('.container');
+
+function userCard(jsonObj) {
+  for (var i = 0; i < jsonObj.length; i += 1) {
+    var div = document.createElement('div');
+    container.appendChild(div);
+    var img = document.createElement('img');
+    var avatarUrl = jsonObj[i]['avatar_url'];
+    img.setAttribute('src', avatarUrl);
+    img.setAttribute('height', '150');
+    img.setAttribute('width', '150');
+    img.setAttribute('alt', 'avatar');
+    div.appendChild(img);
+    var type = document.createElement('p');
+    type.innerHTML = `Тип учетной записи:<br/>${jsonObj[i]['type']}`;
+    div.appendChild(type);
+    var ref = document.createElement('a');
+    var userPage = jsonObj[i]['html_url'];
+    ref.setAttribute('href', userPage);
+    ref.innerHTML = `Открыть профиль`
+    div.appendChild(ref);
+  };
 };
