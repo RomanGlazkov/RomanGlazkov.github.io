83a1fc9b38ab83ff116dc65b9cd3ce7dfebe82a2
RomanGlazkov <66161402+RomanGlazkov@users.noreply.github.com>
2020-06-01 12:43:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,23 +11,57 @@ request.onload = function() {
 var container = document.querySelector('.container');
 
 function userCard(jsonObj) {
-  for (var i = 0; i < jsonObj.length; i += 1) {
-    var div = document.createElement('div');
-    container.appendChild(div);
-    var img = document.createElement('img');
-    var avatarUrl = jsonObj[i]['avatar_url'];
-    img.setAttribute('src', avatarUrl);
-    img.setAttribute('height', '150');
-    img.setAttribute('width', '150');
-    img.setAttribute('alt', 'avatar');
-    div.appendChild(img);
-    var type = document.createElement('p');
-    type.innerHTML = `Тип учетной записи:<br/>${jsonObj[i]['type']}`;
-    div.appendChild(type);
-    var ref = document.createElement('a');
-    var userPage = jsonObj[i]['html_url'];
-    ref.setAttribute('href', userPage);
-    ref.innerHTML = `Открыть профиль`
-    div.appendChild(ref);
+  var pagination = document.querySelector('.pagination');
+  var notesOnPage = 9;
+  var countOfPages = Math.ceil(jsonObj.length / notesOnPage);
+  var items = [];
+  for (var i = 1; i <= countOfPages; i += 1) {
+    var li = document.createElement('li');
+    li.innerHTML = i;
+    pagination.appendChild(li);
+    items.push(li);
   };
+  
+  showPage(items[0]);
+
+  for (var item of items) {
+    item.addEventListener('click', function() {
+      showPage(this);
+    })
+  };
+
+  function showPage(item) {
+    var active = document.querySelector('.active');
+    if (active) {
+      active.classList.remove('active');
+    };
+    item.classList.add('active');
+    var pageNum = +item.innerHTML;
+    var start = (pageNum - 1) * notesOnPage;
+    var end = start + notesOnPage;
+    container.innerHTML = '';
+    for (var i = start; i < end; i += 1) {
+      if (i >= jsonObj.length) {
+        break;
+      };
+      var div = document.createElement('div');
+      div.classList.add('cards');
+      container.appendChild(div);
+      var img = document.createElement('img');
+      var avatarUrl = jsonObj[i]['avatar_url'];
+      img.setAttribute('src', avatarUrl);
+      img.setAttribute('height', '150');
+      img.setAttribute('width', '150');
+      img.setAttribute('alt', 'avatar');
+      div.appendChild(img);
+      var type = document.createElement('p');
+      type.innerHTML = `Тип учетной записи:<br/>${jsonObj[i]['type']}`;
+      div.appendChild(type);
+      var ref = document.createElement('a');
+      var userPage = jsonObj[i]['html_url'];
+      ref.setAttribute('href', userPage);
+      ref.innerHTML = `Открыть профиль`
+      div.appendChild(ref);
+    };
+  }; 
 };
