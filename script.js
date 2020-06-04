d863de4837a18c3cd67de2c8e0cd8806cf4058cc
RomanGlazkov <66161402+RomanGlazkov@users.noreply.github.com>
2020-06-04 11:35:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,78 @@
-var requestURL = 'https://api.github.com/users';
+var since = [0];
+var getId;
+var per_page = 9;
+var requestURL = 'https://api.github.com/users?since=' + since[0] + '&per_page=' + per_page;
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
+var next = 0;
 request.onload = function() {
   var users = request.response;
   userCard(users);
 };
 
-var container = document.querySelector('.container');
-
 function userCard(jsonObj) {
-  var pagination = document.querySelector('.pagination');
-  var notesOnPage = 6;
-  var countOfPages = Math.ceil(jsonObj.length / notesOnPage);
-  var items = [];
-  for (var i = 1; i <= countOfPages; i += 1) {
-    var li = document.createElement('li');
-    li.innerHTML = i;
-    pagination.appendChild(li);
-    items.push(li);
-  };
-  
-  showPage(items[0]);
-
-  for (var item of items) {
-    item.addEventListener('click', function() {
-      showPage(this);
-    })
-  };
-
-  function showPage(item) {
-    var active = document.querySelector('.active');
-    if (active) {
-      active.classList.remove('active');
-    };
-    item.classList.add('active');
-    var pageNum = +item.innerHTML;
-    var start = (pageNum - 1) * notesOnPage;
-    var end = start + notesOnPage;
-    container.innerHTML = '';
-    for (var i = start; i < end; i += 1) {
-      if (i >= jsonObj.length) {
-        break;
-      };
-      var div = document.createElement('div');
-      div.classList.add('cards');
-      container.appendChild(div);
-      var img = document.createElement('img');
-      var avatarUrl = jsonObj[i]['avatar_url'];
-      img.setAttribute('src', avatarUrl);
-      img.setAttribute('height', '150');
-      img.setAttribute('width', '150');
-      img.setAttribute('alt', 'avatar');
-      div.appendChild(img);
-      var type = document.createElement('p');
-      type.innerHTML = `Тип учетной записи:<br/>${jsonObj[i]['type']}`;
-      div.appendChild(type);
-      var ref = document.createElement('a');
-      var userPage = jsonObj[i]['html_url'];
-      ref.setAttribute('href', userPage);
-      ref.innerHTML = `Открыть профиль`
-      div.appendChild(ref);
-    };
+  var container = document.querySelector('.container');
+  container.innerHTML = '';
+  getId = jsonObj[per_page - 1]['id'];
+  if (next === since.length - 1) {
+    since.push(getId); 
+  }
+  for (var i = 0; i < per_page; i += 1) {
+    var div = document.createElement('div');
+    div.classList.add('cards');
+    container.appendChild(div);
+    var img = document.createElement('img');
+    var avatarUrl = jsonObj[i]['avatar_url'];
+    img.setAttribute('src', avatarUrl);
+    img.setAttribute('height', '150');
+    img.setAttribute('width', '150');
+    img.setAttribute('alt', 'avatar');
+    div.appendChild(img);
+    var type = document.createElement('p');
+    type.classList.add('types');
+    type.innerHTML = `Тип учетной записи:<br/>${jsonObj[i]['type']}`;
+    div.appendChild(type);
+    var ref = document.createElement('a');
+    var userPage = jsonObj[i]['html_url'];
+    ref.setAttribute('href', userPage);
+    ref.innerHTML = `Открыть профиль`
+    div.appendChild(ref);
   }; 
+};
+
+var buttonBack = document.querySelector("#buttonBack");
+buttonBack.disabled = true;
+var buttonNext = document.querySelector("#buttonNext");
+
+buttonNext.onclick = function() {
+  next = next + 1;
+  buttonBack.disabled = false;
+  var requestURL = 'https://api.github.com/users?since=' + since[next] + '&per_page=' + per_page;
+  var request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    var users = request.response;
+    userCard(users);
+  };
+};
+
+buttonBack.onclick = function() {
+  if (next === 1) {
+    buttonBack.disabled = true;
+  };
+  next = next - 1;
+  next = Math.max(next, 0)
+  var requestURL = 'https://api.github.com/users?since=' + since[next] + '&per_page=' + per_page;
+  var request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    var users = request.response;
+    userCard(users);
+  };
 };
